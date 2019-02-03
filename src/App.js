3eae4eaa5d6ebabe3bde1f188c34653d0169119f
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-03 14:55:10 -0500

    Added a modal for adding tools

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Login from "./containers/Login";
 import Register from "./containers/Register";
 import Dashboard from "./containers/Dashboard";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import red from "@material-ui/core/colors/red";
+import lightGreen from "@material-ui/core/colors/lightGreen";
 
 const theme = createMuiTheme({
   palette: {
-    primary: { main: red[100] } // Purple and green play nicely together.
+    primary: { main: lightGreen[500] },
+    type: "dark"
   },
   typography: { useNextVariants: true }
 });
