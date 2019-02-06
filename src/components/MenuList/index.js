ddcb3e1e78fd1d01832c02bcd43c9cac94d2d3f4
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-06 10:23:38 -0500

    Fixed the profile cards margin

--- src/components/MenuList/index.js
+++ src/components/MenuList/index.js
@@ -3,10 +3,10 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import DashboardIcon from "@material-ui/icons/Dashboard";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+// import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import PeopleIcon from "@material-ui/icons/People";
 import BarChartIcon from "@material-ui/icons/BarChart";
-import MoneyIcon from "@material-ui/icons/Money";
+// import MoneyIcon from "@material-ui/icons/Money";
 import HomeIcon from "@material-ui/icons/Home";
 import { Link } from "react-router-dom";
 export const mainListItems = (
